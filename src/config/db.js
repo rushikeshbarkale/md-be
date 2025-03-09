7580aeaf79420d3ab7bc5f426b3608dedccde19d
Rushikesh Barkale <rushikesh@turnkey.work>
2025-03-09 15:55:58 +0530

    updated db connection

--- src/config/db.js
+++ src/config/db.js
@@ -3,13 +3,32 @@ const { Pool } = require("pg");
 // require("dotenv").config();
 require("dotenv").config({ path: ".env.local" });
 
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
+const isProduction = process.env.ENV_TYPE === "production";
+
+// const pool = new Pool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+// });
+
+const pool = new Pool(
+  isProduction
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false, // Required for Render
+        },
+      }
+    : {
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT,
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        database: process.env.DB_NAME,
+      }
+);
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
